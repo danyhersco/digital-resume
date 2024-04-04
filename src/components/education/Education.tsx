ecbfb52de118dc3910c55411aed9b40127578749
danyhersco <dany.herscovitch@gmail.com>
2024-04-04 01:00:56 +0100

    Added the education items (unfilled)

--- src/components/education/Education.tsx
+++ src/components/education/Education.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Education.css'
+import EducationItem from './EducationItem';
+
 
 
 const Education: React.FC = () => {
   return(
     <div className="education-section">
-      <h1 className="education-h1">Education</h1>
+      <div className="education-zone">
+        <h1 className="education-h1">Education</h1>
+        <EducationItem/>
+        <EducationItem/>
+        <EducationItem/>
+        <EducationItem/>
+      </div>
     </div>
   )
 }
---
+++ src/components/education/EducationItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './EducationItem.css';
+
+
+const EducationItem: React.FC = () => {
+  return (
+    <div className="education-item">
+      London School of Economics
+    </div>
+  )
+}
+
+export default EducationItem;
