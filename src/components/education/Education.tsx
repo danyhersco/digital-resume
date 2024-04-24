00d9c0de7678b69f21559aca3914505b3f5012e0
danyhersco <dany.herscovitch@gmail.com>
2024-04-25 00:57:48 +0100

    Made education section look better (coloured logos, text position, etc

--- src/components/education/Education.tsx
+++ src/components/education/Education.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import './Education.css'
-import EducationItem from './EducationItem';
-
-
+import React from "react";
+import "./Education.css";
+import EducationItem from "./EducationItem";
 
 const Education: React.FC = () => {
-  return(
+  return (
     <div className="education-section">
       <div className="education-zone">
         <h1 className="education-h1">Education</h1>
-        <EducationItem/>
-        <EducationItem/>
-        <EducationItem/>
-        <EducationItem/>
+        <EducationItem
+          institutionName="Imperial College London"
+          educationLevel="Master's Degree"
+          programmeName="MSc Computational Science and Engineering"
+        />
+        <EducationItem
+          institutionName="London School of Economics"
+          educationLevel="Bachelor's Degree"
+          programmeName="Bsc Management"
+        />
+        <EducationItem
+          institutionName="Athénée Ganenou"
+          educationLevel="High School Degree"
+          programmeName="Mathematics Advanced"
+        />
+        <EducationItem
+          institutionName="Le Wagon"
+          educationLevel="Bootcamp"
+          programmeName="Data Science"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Education;
--- src/components/education/EducationItem.tsx
+++ src/components/education/EducationItem.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
-import './EducationItem.css';
+import React from "react";
+import "./EducationItem.css";
+import { ReactComponent as ImperialLogo } from "../../images/imperial_logo.svg";
+import { ReactComponent as LseLogo } from "../../images/lse_logo.svg";
+import { ReactComponent as GanenouLogo } from "../../images/ganenou_logo.svg";
+import { ReactComponent as LeWagonLogo } from "../../images/lewagon_logo.svg";
+import { ReactComponent as UnknownLogo } from "../../images/unknown_logo.svg";
 
-
-const EducationItem: React.FC = () => {
-  return (
-    <div className="education-item">
-      London School of Economics
-    </div>
-  )
+interface EducationItemProps {
+  institutionName: string;
+  educationLevel: string;
+  programmeName: string;
 }
+
+const EducationItem: React.FC<EducationItemProps> = ({
+  institutionName,
+  educationLevel,
+  programmeName,
+}) => {
+
+  let logoComponent = null;
+
+  switch (institutionName) {
+    case "Imperial College London":
+      logoComponent = <ImperialLogo className="institution-logo imperial-logo"/>;
+      break;
+    case "London School of Economics":
+      logoComponent = <LseLogo className="institution-logo lse-logo"/>;
+      break;
+    case "Athénée Ganenou":
+      logoComponent = <GanenouLogo className="institution-logo ganenou-logo"/>;
+      break;
+    case "Le Wagon":
+      logoComponent = <LeWagonLogo className="institution-logo lewagon-logo"/>;
+      break;
+    default:
+      logoComponent = <UnknownLogo className="institution-logo unknown-logo"/>;
+  }
+
+  return (
+    <div className="education-item-zone">
+      {logoComponent}
+      <div className="education-item">
+        <h2>{institutionName}</h2>
+        <p>
+          {educationLevel} - {programmeName}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default EducationItem;
