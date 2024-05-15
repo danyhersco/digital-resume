6ba6607b48268194a2985894c51edf1f36dc45b9
danyhersco <dany.herscovitch@gmail.com>
2024-05-15 22:39:32 +0100

    Added activities data + layout for activity item

--- src/components/activities/Activities.tsx
+++ src/components/activities/Activities.tsx
@@ -2,22 +2,49 @@ import React from 'react';
 import ArrowLeft from "../../images/arrow_left.svg";
 import ArrowRight from "../../images/arrow_right.svg";
 import "./Activities.css";
-
+import activitiesDataMapping from './ActivitiesData';
 
 const Activities: React.FC = () => {
+  const activitiesData = activitiesDataMapping["Piano"]
+
+  const formatDescription = (description: string) => {
+    return description.split('\n').map((line, index) => (
+      <span key={index}>
+        {line}
+        <br />
+      </span>
+    ));
+  };
+
+
   return (
     <div className="activities-section">
       <div className="activities-zone">
         <h1 className="activities-h1">Activities</h1>
-        <div className="left-arrow-zone">
-          <img src={ArrowLeft} alt="Left Arrow" />
-        </div>
-        <div className="activities-item">
-          <h2 className="activity-title">Independant Graphic Designer</h2>
-          <h2 className="activity-span">Since 2014</h2>
-        </div>
-        <div className="right-arrow-zone">
-          <img src={ArrowRight} alt="Right Arrow"/>
+        <div className="activities-carousel">
+
+          <div className="arrow-zone">
+            <img src={ArrowLeft} alt="Left Arrow"/>
+          </div>
+
+          <div className="activities-item">
+            <h2 className="activity-title">{activitiesData.activity}</h2>
+            <h2 className="activity-span">
+              {activitiesData.startDate} - {activitiesData.endDate}
+            </h2>
+            <p className="activity-description">
+              {formatDescription(activitiesData.description)}
+            </p>
+            <div className="activity-gallery">
+              <div className="gallery-element"></div>
+              <div className="gallery-element"></div>
+            </div>
+          </div>
+
+          <div className="arrow-zone">
+            <img src={ArrowRight} alt="Right Arrow"/>
+          </div>
+
         </div>
       </div>
     </div>
---
+++ src/components/activities/ActivitiesData.ts
@@ -0,0 +1,26 @@
+export { };
+
+interface ActivitiesData {
+  activity: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+  gallery: string[];
+}
+
+const activitiesDataMapping: Record<string, ActivitiesData> = {
+  Piano: {
+    activity: "Piano",
+    startDate: "09/2007",
+    endDate: "Present",
+    description: "Growing up in a musical family, I started playing the piano at age 6. For nine " +
+    "years in Belgium, I took weekly classical music lessons and practiced daily for hours. My " +
+    "teacher, Hagit, is well-known in the classical music scene and runs Musica Mundi, one of " +
+    "the world's top music schools. I performed at their private concerts twice a year.\n\n" +
+    "Today, I still play regularly and have branched out into other genres. Currently, I'm " +
+    "learning to improvise in jazz.",
+    gallery: ["link 1", "link 2", "link 3"],
+  }
+}
+
+export default activitiesDataMapping;
