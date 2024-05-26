f8cd5e80dae3920348092addce6a48775b96a3ce
danyhersco <dany.herscovitch@gmail.com>
2024-05-26 11:13:21 -0400

    added scroll snap through sections

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,12 @@ function App() {
   return (
     <div className="App">
       <NavigationBar/>
-      <WelcomePage/>
-      <Overview/>
-      <Education/>
-      <WorkExperience/>
-      <Activities/>
-      <Skills/>
+      <section id="welcome"><WelcomePage/></section>
+      <section id="overview"><Overview/></section>
+      <section id="education"><Education/></section>
+      <section id="work_experience"><WorkExperience/></section>
+      <section id="activities"><Activities/></section>
+      <section id="skills"><Skills/></section>
     </div>
   );
 }
