846d7d33bb321935a019e07c2d5d05fe583aed5a
danyhersco <dany.herscovitch@gmail.com>
2024-05-28 12:00:43 -0400

    Added input bar and print when submitting query

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,20 +1,46 @@
-import React from 'react';
-import './Chat.css'
+import React, { useState } from "react";
+import "./Chat.css";
+import { ReactComponent as Arrow } from '../../images/arrow_right.svg';
 
 const Chat: React.FC = () => {
+  const [input, setInput] = useState<string>("");
+  const [query, setQuery] = useState<string | null>(null);
+
+  const handleSubmit = () => {
+    setQuery(input);
+    setInput('');
+  }
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleSubmit();
+  };
+
   return (
     <div className="chat-section">
       <div className="chat-zone">
-        <h1 className="chat-h1">
-          Chat With Me
-        </h1>
-        <div className="chat-bubbles">
-          Dialogue here
-        </div>
-        <input className="chat-input" placeholder="Your Message Here..." />
+        <h1 className="chat-h1">Chat With Me</h1>
+        <div className="chat-bubbles">{query}</div>
+        <form onSubmit={handleFormSubmit} className="chat-form">
+          <input
+            className="chat-input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Your Message Here..."
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault();
+                handleSubmit();
+              }
+            }}
+          />
+          <button type='submit' className="chat-submit">
+            <Arrow className="submit-arrow"/>
+          </button>
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Chat;
